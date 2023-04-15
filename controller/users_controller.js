50d1acd3b41d3884757ac7d23a17683ae0e975bb
Md Umar <mohammadumar389@gmail.com>
2023-04-16 00:29:54 +0530

    Twilio Messaging

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -739,20 +739,23 @@ module.exports.applyLeave = async (req, res) => {
     //send message to parents with the link-: http://localhost:3000/leave/:leave_id-:
     const accountSid = process.env.ACCOUNT_SID;
     const authToken = process.env.AUTH_TOKEN;
+    const twNumber = process.env.TWN_NO;
     const client = require("twilio")(accountSid, authToken);
-    console.log(newLeave.id);
-    const msgBody = `Your ward ${req.user.name} has request for leave, please verify and respond on our website accordingly https://smit-hms.vercel.app/leave/${newLeave.id}`;
+    console.log(newLeave._id);
+    const msgBody = `Your ward ${req.user.name} has request for leave, please verify and respond on our website accordingly https://smit-hms.vercel.app/leave/${newLeave._id}`;
     client.messages
       .create({
-        body: msgBody,
-        from: "+1(620) 646-4490",
-        to: "+919939329441",
+        body: `${msgBody}`,
+        from: twNumber,
+        to: number,
       })
-      .then((message) => console.log(message.sid));
-    return res.status(200).json({
-      message:
-        "Leave applied and link with message is send to your parents contact number",
-    });
+      .then((mes) => {
+        console.log(mes);
+        return res.status(200).json({
+          message:
+            "Leave applied and link with message is send to your parents contact number",
+        });
+      });
   } catch (error) {
     console.log(error);
     return res.status(501).json({
